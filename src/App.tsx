9bd965e1f4c07f1f16e06cb022106fb95b31675e
torie28 <pastoriejoe18@gmail.com>
2025-11-11 13:34:19 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl font-bold">
+      🚀 React + TypeScript + Tailwind + SWC is Ready!
+    </div>
+  );
+}
+
+export default App;
