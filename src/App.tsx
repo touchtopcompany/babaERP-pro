e6f59b532d100840864cda944ac3bd17ed446c80
torie28 <pastoriejoe18@gmail.com>
2025-11-12 08:26:28 +0300

    first comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl font-bold">
-      🚀 React + TypeScript + Tailwind + SWC is Ready!
+      🚀 welcome to babaERP pro 
     </div>
   );
 }
