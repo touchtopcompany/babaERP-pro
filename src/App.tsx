2408101c3e6e462a36cc33855c0d015a704ff3df
torie28 <pastoriejoe18@gmail.com>
2025-11-12 16:54:54 +0300

    simple file modifications

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl font-bold">
-      🚀 welcome to babaERP pro 
-    </div>
-  );
-}
-
-export default App;
